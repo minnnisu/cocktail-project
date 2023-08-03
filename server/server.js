c02e5a1a856efaa97129c4dfe6e073ab8ff5f20b
minnnisu <mine52100@naver.com>
2023-08-03 23:55:50 +0900

    Fix and Add: Fix api code and add a cocktail image upload function

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+var cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./config/mongodb");
-var cors = require("cors");
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const port = 8080;
 const uri = db.MongoURI;
 
+// 이미지 업로드를 위한 설정
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "static/images/cocktails"); // 업로드된 파일을 저장할 디렉토리 경로
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname)); // 저장될 파일명 설정
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// 알코올 값 추가
+// 정적 파일 제공
+//사용자가 127.0.0.1:3000/images/cat.jpg 로 접근한다면, 해당 파일을 public/images/cat.jpg에 존재하는지 검색한다.
+app.use(express.static("public"));
 
 // 새로운 base-spirit-type 추가(테스트 완료)
 app.post("/base-spirit-type", async (req, res) => {
@@ -26,11 +43,10 @@ app.post("/base-spirit-type", async (req, res) => {
       .collection("base_spirit_type")
       .findOne({ "name.en": newBaseSpiritType.name.en });
     if (cocktailName !== null) {
-      res.status(403).json({
+      return res.status(403).json({
         error: "validationError",
         message: "This base spirit type already exists.",
       });
-      return;
     }
 
     await database.collection("base_spirit_type").insertOne({
@@ -39,6 +55,8 @@ app.post("/base-spirit-type", async (req, res) => {
     });
 
     res.status(201).send();
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
@@ -57,11 +75,10 @@ app.post("/base-spirit", async (req, res) => {
       .collection("base_spirit")
       .findOne({ "name.en": newBaseSpirit.name.en });
     if (cocktailName !== null) {
-      res.status(403).json({
+      return res.status(403).json({
         error: "validationError",
         message: "This base spirit type already exists.",
       });
-      return;
     }
 
     await database.collection("base_spirit").insertOne({
@@ -82,6 +99,8 @@ app.post("/base-spirit", async (req, res) => {
     await database.collection("base_spirit_type").updateOne(filter, update);
 
     res.status(201).send();
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
@@ -98,6 +117,8 @@ app.get("/base-spirit-type", async (req, res) => {
       .toArray();
 
     res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
@@ -111,6 +132,8 @@ app.get("/base-spirit", async (req, res) => {
     const result = await database.collection("base_spirit").find({}).toArray();
 
     res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
@@ -125,23 +148,31 @@ app.get("/cocktail/name/validation", async (req, res) => {
       .collection("cocktail")
       .findOne({ "name.en": req.query.name });
     if (cocktailName !== null) {
-      res.status(403).json({
+      return res.status(403).json({
         error: "validationError",
         message: "This cocktail already exists.",
       });
-      return;
     }
 
     res.status(200).send();
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
 });
 
 // 칵테일 추가(테스트 완료)
-app.post("/cocktail", async (req, res) => {
-  const cocktail = req.body;
+app.post("/cocktail", upload.single("image"), async (req, res) => {
+  const cocktail = JSON.parse(req.body.data);
   const client = new MongoClient(uri);
+
+  if (!req.file) {
+    return res.status(400).send({ message: "please upload with image" });
+  }
+
+  console.log(cocktail);
+
   try {
     const database = client.db("cocktail_project");
 
@@ -149,16 +180,15 @@ app.post("/cocktail", async (req, res) => {
       .collection("cocktail")
       .findOne({ "name.en": cocktail.name.en });
     if (cocktailName !== null) {
-      res.status(403).json({
+      return res.status(403).json({
         error: "validationError",
         message: "This cocktail already exists.",
       });
-      return;
     }
 
     await Promise.all(
       cocktail.ingredients.map(async (ingredient, index) => {
-        const filter = { "name.en": ingredient.base_spirit_name };
+        const filter = { "name.en": ingredient.base_spirit_name.en };
         const update = {
           $push: {
             cocktails: {
@@ -173,6 +203,8 @@ app.post("/cocktail", async (req, res) => {
 
     await database.collection("cocktail").insertOne(cocktail);
     res.status(201).send();
+  } catch (error) {
+    res.status(500).send();
   } finally {
     await client.close();
   }
