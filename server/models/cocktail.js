0119bff76ca04d4a82d181d6c618ae023742eb51
minnnisu <mine52100@naver.com>
2023-08-22 20:32:37 +0900

    Fix: modify schema

--- server/models/cocktail.js
+++ server/models/cocktail.js
@@ -4,28 +4,36 @@ const Schema = require("mongoose").Schema;
 // Define Schemes
 const CocktailSchema = new Schema(
   {
-    name: { type: String, unique: true, sparse: true },
+    name: { type: String, required: true, unique: true, sparse: true },
     alcohol: [
       {
-        alcoholID: { type: Schema.Types.ObjectId, ref: "Alcohol" },
-        name: { type: String, unique: true, sparse: true },
+        alcoholID: {
+          type: Schema.Types.ObjectId,
+          ref: "Alcohol",
+          required: true,
+        },
+        name: { type: String, required: true, unique: true, sparse: true },
         subAlcoholName: String,
-        volume: Number,
-        unit: String,
+        volume: { type: Number, required: true },
+        unit: { type: String, required: true },
       },
     ],
     nonAlcohol: [
       {
-        nonAlcoholID: { type: Schema.Types.ObjectId, ref: "NonAlcohol" },
-        name: { type: String, unique: true, sparse: true },
-        volume: Number,
-        unit: String,
+        nonAlcoholID: {
+          type: Schema.Types.ObjectId,
+          ref: "NonAlcohol",
+          required: true,
+        },
+        name: { type: String, required: true, unique: true, sparse: true },
+        volume: { type: Number, required: true },
+        nit: { type: String, required: true },
       },
     ],
-    taste: Array,
-    garnish: Array,
-    recipe: String,
-    image_url: String,
+    taste: { type: Array, required: true },
+    garnish: { type: Array, required: true },
+    recipe: { type: String, required: true },
+    image_url: { type: String, required: true },
   },
   {
     versionKey: false,
