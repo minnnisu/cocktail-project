e29807954f6637f87b2ce4549a220a8dc45030d3
minnnisu <mine52100@naver.com>
2023-08-23 16:27:59 +0900

    Fix and Add: Fix and Add logic

--- server/api/routes/alcoholRouter.js
+++ server/api/routes/alcoholRouter.js
@@ -1,59 +1,151 @@
 const AlcoholService = require("../../service/AlcoholService");
 const alcoholModel = require("../../models/alcohol");
+const nonAlcoholModel = require("../../models/nonAlcohol");
+const cocktailModel = require("../../models/cocktail");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  AlcoholService(alcoholModel)
-    .getAlcohol(req.query)
-    .then((data) => res.status(200).send(data))
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
-});
+router
+  .get("/alcohol", (req, res, next) => {
+    AlcoholService({ alcoholModel })
+      .getAlcohol(req.query)
+      .then((data) => res.status(200).send(data))
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .post("/alcohol", (req, res, next) => {
+    AlcoholService({ alcoholModel })
+      .addAlcohol(req.body)
+      .then((result) => {
+        res.status(201).send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .put("/alcohol", (req, res, next) => {
+    AlcoholService({ alcoholModel })
+      .updateAlcohol(req.query, req.body)
+      .then((doc) => {
+        if (doc === null) {
+          return res.status(403).send("조건에 만족하는 데이터가 없습니다");
+        }
+        res.status(200).send();
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .delete("/alcohol", (req, res, next) => {
+    AlcoholService({ alcoholModel })
+      .deleteAlcohol(req.query)
+      .then((doc) => {
+        if (doc === null) {
+          return res.status(403).send("조건에 만족하는 데이터가 없습니다");
+        }
+        res.status(200).send();
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  });
 
-router.post("/", (req, res, next) => {
-  AlcoholService(alcoholModel)
-    .addAlcohol(req.body)
-    .then((result) => {
-      console.log(result);
-      res.status(201).send();
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
-});
+router
+  .get("/non-alcohol", (req, res, next) => {
+    AlcoholService({ nonAlcoholModel })
+      .getNonAlcohol(req.query)
+      .then((data) => {
+        console.log(data);
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .post("/non-alcohol", (req, res, next) => {
+    AlcoholService({ nonAlcoholModel })
+      .addNonAlcohol(req.body)
+      .then((result) => {
+        res.status(201).send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .put("/non-alcohol", (req, res, next) => {
+    AlcoholService({ nonAlcoholModel })
+      .updateNonAlcohol(req.query, req.body)
+      .then((doc) => {
+        if (doc === null) {
+          return res.status(403).send("조건에 만족하는 데이터가 없습니다");
+        }
+        res.status(200).send();
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .delete("/non-alcohol", (req, res, next) => {
+    NonAlcoholService({ nonAlcoholModel })
+      .deleteNonAlcohol(req.query)
+      .then((doc) => {
+        if (doc === null) {
+          return res.status(403).send("조건에 만족하는 데이터가 없습니다");
+        }
+        res.status(200).send();
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  });
 
-router.put("/", (req, res, next) => {
-  AlcoholService(alcoholModel)
-    .updateAlcohol(req.query, req.body)
-    .then((doc) => {
-      if (doc === null) {
-        return res.status(403).send("조건에 만족하는 데이터가 없습니다");
-      }
-      res.status(200).send();
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
-});
+router
+  .get("/cocktail", (req, res, next) => {
+    AlcoholService({ cocktailModel })
+      .getCocktail(req.query)
+      .then((data) => {
+        console.log(data);
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
+  .post("/cocktail", (req, res, next) => {
+    AlcoholService({ alcoholModel, nonAlcoholModel, cocktailModel })
+      .addCocktail(req.body)
+      .then((result) => {
+        console.log(result);
+        res.status(201).send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  });
 
-router.delete("/", (req, res, next) => {
-  AlcoholService(alcoholModel)
-    .deleteAlcohol(req.query)
-    .then((doc) => {
-      if (doc === null) {
-        return res.status(403).send("조건에 만족하는 데이터가 없습니다");
-      }
-      res.status(200).send();
-    })
-    .catch((err) => {
-      console.error(err);
-      next(err);
-    });
+// error handling 미들웨어
+router.use((err, req, res, next) => {
+  if (
+    err.name === "ValidationError" ||
+    (err.name === "MongoServerError" && err.code === 11000) // unique키 중복
+  ) {
+    return res.status(403).json({ ValidationError: err.message });
+  } else if (err.name === "MongoError") {
+    return res.status(400).json({ message: err.message });
+  }
+
+  next();
 });
 
 module.exports = router;
--- server/service/alcoholService.js
+++ server/service/alcoholService.js
@@ -1,5 +1,7 @@
-function AlcoholService(alcoholModel) {
-  function getFilter(filter) {
+const ValidationError = require("./ErrorHandler");
+
+function AlcoholService({ alcoholModel, nonAlcoholModel, cocktailModel }) {
+  function makeFilter(filter) {
     const { id, name } = filter;
     const processedFilter = {};
     if (id) {
@@ -14,91 +16,180 @@ function AlcoholService(alcoholModel) {
   }
 
   async function addAlcohol(data) {
-    const { name, abv, subAlcohol } = data;
-
     const alcohol = new alcoholModel();
-    alcohol.name = name;
-    alcohol.abv = abv;
-    alcohol.subAlcohol = subAlcohol;
 
-    if (subAlcohol) {
-      const subAlcoholArray = subAlcohol.map((item) => item.name);
-      const subAlcoholset = new Set(subAlcoholArray);
-      console.log(subAlcoholArray);
-      console.log(subAlcoholset);
-      if (subAlcoholArray.length !== subAlcoholset.size) {
-        throw new Error(`${name}의 하위 술들 중 중복된 것이 있습니다.`);
+    alcohol.name = data.name;
+
+    if (data.subAlcohols && data.subAlcohols.length > 0) {
+      if (data.abv) {
+        throw new ValidationError(
+          "subAlcohols과 abv는 둘 중 하나만 존재해야합니다."
+        );
       }
+
+      const subAlcoholNames = data.subAlcohols.map((subAlcohol) => {
+        return subAlcohol.name;
+      });
+      const subAlcoholNameSet = new Set(subAlcoholNames);
+      if (subAlcoholNames.length !== subAlcoholNameSet.size) {
+        throw new ValidationError(
+          `${data.name}의 하위 술 중에서 중복된 것이 있습니다.`
+        );
+      }
+      alcohol.subAlcohols = data.subAlcohols;
+    } else {
+      if (!data.abv) {
+        throw new ValidationError("abv의 값이 없습니다.");
+      }
+
+      alcohol.abv = data.abv;
+      alcohol.subAlcohols = undefined;
     }
 
-    return alcohol
-      .save()
-      .then((savedData) => {
-        console.log("Data saved successfully:", savedData);
-      })
-      .catch((error) => {
-        if (error.name === "ValidationError") {
-          console.error("Validation error:", error.message);
-        } else if (error.name === "MongoError" && error.code === 11000) {
-          console.error("Duplicate key error:", error.message);
-        } else {
-          console.error("Error saving data:", error);
-        }
-      });
+    return alcohol.save();
   }
 
   async function getAlcohol(qurey) {
-    return alcoholModel.find(getFilter(qurey));
+    return alcoholModel.find(makeFilter(qurey)).exec();
   }
 
-  async function updateAlcohol(filter, update) {
-    return alcoholModel.findOneAndUpdate(
-      getFilter(filter),
-      { ...update },
-      { upsert: false }
-    );
-  }
+  // async function updateAlcohol(filter, update) {
+  //   return alcoholModel.findOneAndUpdate(
+  //     makeFilter(filter),
+  //     { ...update },
+  //     { upsert: false }
+  //   );
+  // }
 
-  async function deleteAlcohol(filter) {
-    return alcoholModel.findOneAndDelete(getFilter(filter));
-  }
+  // async function deleteAlcohol(filter) {
+  //   return alcoholModel.findOneAndDelete(makeFilter(filter));
+  // }
 
   // ---
 
   async function addNonAlcohol(data) {
-    const { name } = data;
-
     const nonAlcohol = new nonAlcoholModel();
-    nonAlcohol.name = name;
+    nonAlcohol.name = data.name;
 
     return nonAlcohol.save();
   }
 
   async function getNonAlcohol(qurey) {
-    return nonAlcoholModel.find(getFilter(qurey));
+    return nonAlcoholModel.find(makeFilter(qurey)).exec();
   }
 
-  async function updateNonAlcohol(filter, update) {
-    return nonAlcoholModel.findOneAndUpdate(
-      getFilter(filter),
-      { ...update },
-      { upsert: false }
-    );
-  }
+  // async function updateNonAlcohol(filter, update) {
+  //   return nonAlcoholModel.findOneAndUpdate(
+  //     makeFilter(filter),
+  //     { ...update },
+  //     { upsert: false }
+  //   );
+  // }
 
-  async function deleteNonAlcohol(filter) {
-    return nonAlcoholModel.findOneAndDelete(getFilter(filter));
-  }
+  // async function deleteNonAlcohol(filter) {
+  //   return nonAlcoholModel.findOneAndDelete(makeFilter(filter));
+  // }
 
   // ---
 
-  async function addCocktail(cocktailData) {
-    const cocktail = new cocktailModel(cocktailData);
-    return cocktail.save();
+  async function addCocktail(data) {
+    const cocktail = new cocktailModel(data);
+    cocktail.name = data.name;
+    cocktail.tastes = data.tastes;
+    cocktail.garnishes = data.garnishes;
+    cocktail.recipe = data.recipe;
+    cocktail.image_url = data.image_url;
+
+    cocktail.alcohols = await Promise.all(
+      data.alcohols.map(async (alcohol) => {
+        const alcoholDocs = await getAlcohol({ name: alcohol.name });
+        if (alcoholDocs.length < 1) {
+          throw new ValidationError(
+            `${alcohol.name}에 대한 데이터가 등록되어 있지 않습니다.`
+          );
+        }
+
+        const targetAlcoholDoc = alcoholDocs[0];
+        alcohol.alcoholID = targetAlcoholDoc._id;
+
+        if (alcohol.subAlcoholName) {
+          for (const targetSubAlcohol of targetAlcoholDoc.subAlcohols) {
+            if (targetSubAlcohol.name === alcohol.subAlcoholName) {
+              return alcohol;
+            }
+          }
+
+          throw new ValidationError(
+            `${alcohol.subAlcoholName}에 대한 데이터가 등록되어 있지 않습니다.`
+          );
+        }
+
+        return alcohol;
+      })
+    );
+
+    cocktail.nonAlcohols = await Promise.all(
+      data.nonAlcohols.map(async (nonAlcohol) => {
+        const nonAlcoholDocs = await getNonAlcohol({ name: nonAlcohol.name });
+
+        const targetNonAlcoholDoc = nonAlcoholDocs[0];
+        nonAlcohol.nonAlcoholID = targetNonAlcoholDoc._id;
+
+        return nonAlcohol;
+      })
+    );
+    const savedCocktail = await cocktail.save();
+
+    await Promise.all(
+      cocktail.alcohols.map(async (alcohol) => {
+        if (alcohol.subAlcoholName) {
+          const filter = {
+            _id: alcohol.alcoholID,
+            "subAlcohols.name": alcohol.subAlcoholName,
+          };
+          const update = {
+            $push: {
+              "subAlcohols.$.cocktails": cocktail._id,
+            },
+          };
+          await alcoholModel.findOneAndUpdate(filter, update, {
+            upsert: false,
+          });
+
+          return;
+        }
+
+        const filter = {
+          _id: alcohol.alcoholID,
+        };
+        const update = {
+          $push: {
+            cocktails: cocktail._id,
+          },
+        };
+        await alcoholModel.findOneAndUpdate(filter, update, { upsert: false });
+      })
+    );
+
+    await Promise.all(
+      cocktail.nonAlcohols.map(async (nonAlcohol) => {
+        const filter = { _id: nonAlcohol.nonAlcoholID };
+        const update = {
+          $push: {
+            cocktails: cocktail._id,
+          },
+        };
+        await nonAlcoholModel.findOneAndUpdate(filter, update, {
+          upsert: false,
+        });
+      })
+    );
+
+    return savedCocktail;
   }
 
   async function getCocktail() {
-    return cocktailModel.find();
+    return cocktailModel.find().exec();
   }
 
   async function updateCocktail(filter, update) {
@@ -112,12 +203,8 @@ function AlcoholService(alcoholModel) {
   return {
     addAlcohol,
     getAlcohol,
-    updateAlcohol,
-    deleteAlcohol,
     addNonAlcohol,
     getNonAlcohol,
-    updateNonAlcohol,
-    deleteNonAlcohol,
     addCocktail,
     getCocktail,
     updateCocktail,
