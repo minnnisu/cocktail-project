7849fbc9a2d895ca6e112ef5959581710b61f83f
minnnisu <mine52100@naver.com>
2023-09-07 19:30:34 +0900

    Fix: userRouter 분리(코드 삭제)

--- server/api/routes/authRouter.js
+++ server/api/routes/authRouter.js
@@ -10,7 +10,4 @@ router.post("/logout", authMiddleware.isLoggedIn, authController.logout);
 
 router.post("/signup", authMiddleware.isNotLoggedIn, authController.signup);
 
-// 세션 저장소에 사용자 ID가 있다면 DB를 조회하여 req.user에 정보를 삽입 (deserializeUser() 함수
-router.get("/user", authMiddleware.isLoggedIn, authController.getUser);
-
 module.exports = router;
